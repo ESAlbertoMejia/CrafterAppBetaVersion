da91a4c476e10cfc115eb793620c3e9cb0f8f984
Alberto Mejia <g.a.mejia@outlook.com>
2017-01-06 22:06:52 -0600

    Agregamos RecyclerView y CardView, ademas modificamos sus atributos y se agrega color para el fondo del texto del item del CardView

---
+++ app/src/main/java/com/esamdevelopers/gamej/crafterapp/holders/ContentMainHolder.java
@@ -0,0 +1,15 @@
+package com.esamdevelopers.gamej.crafterapp.holders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by gamej on 6/1/2017.
+ */
+
+public class ContentMainHolder extends RecyclerView.ViewHolder {
+
+    public ContentMainHolder(View itemView) {
+        super(itemView);
+    }
+}
