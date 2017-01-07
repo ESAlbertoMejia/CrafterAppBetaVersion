05203d050edc2c0eac9f0c317eb8ab8f2a67e50c
Alberto Mejia <g.a.mejia@outlook.com>
2017-01-06 22:54:43 -0600

    Agregamos la clase Adapter para el RecyclerView y sus metodos

--- app/src/main/java/com/esamdevelopers/gamej/crafterapp/holders/ContentMainHolder.java
+++ app/src/main/java/com/esamdevelopers/gamej/crafterapp/holders/ContentMainHolder.java
@@ -2,6 +2,10 @@ package com.esamdevelopers.gamej.crafterapp.holders;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.esamdevelopers.gamej.crafterapp.R;
 
 /**
  * Created by gamej on 6/1/2017.
@@ -9,7 +13,13 @@ import android.view.View;
 
 public class ContentMainHolder extends RecyclerView.ViewHolder {
 
+    ImageView img_description;
+    TextView title, description;
+
     public ContentMainHolder(View itemView) {
         super(itemView);
+        img_description = (ImageView) itemView.findViewById(R.id.img_description);
+        title = (TextView) itemView.findViewById(R.id.title_item_card);
+        description = (TextView) itemView.findViewById(R.id.description_item_card);
     }
 }
